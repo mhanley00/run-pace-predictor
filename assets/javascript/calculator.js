c05af91bdd9bf9831a345ed117dbe1327839d3b0
mhanley00 <mehanley2@gmail.com>
2018-09-06 13:22:22 -0400

    updated calculator

--- assets/javascript/calculator.js
+++ assets/javascript/calculator.js
@@ -1,44 +1,57 @@
-// var distanceInput = $("#distance"),
-// 	hoursInput = $("#hours"),
-// 	minutesInput = $("#minutes"),
-// 	secondsInput = $("#seconds"),
-// 	calculateBtn = $("#calculate_btn"),
-// 	paceText = $("#pace");
-
 //   $(".calculate").on("click", function() {
 // 	var miles = parseFloat(distanceInput.value),
 // 		hours = parseFloat(hoursInput.value),
 // 		minutes = parseFloat(minutesInput.value),
 // 		seconds = parseFloat(secondsInput.value);
+//_________________________________________________________________
+//GLOBAL VARIABLES - EXPLANATION BELOW or RIGHT of VAR DECLARATION
+//---------———————————————————————————————————————————–––––––––––––
+// PREVIOUS RUN TIME, DISTANCE, WEATHER
+var pHoursG = $("#p-run-hours").val(); //past run hours from user input
+var pMinutesG = $("#p-run-mins").val(); //past run minutes from user input
+var pSecondsG = $("#p-run-secs").val(); //past run secons from user input
+var pRunTimeG = (pHoursG * 60 * 60) + (pMinutesG * 60) + (pSeconds); // past run time from user input
+var pRunDistG = $("#p-run-dist").val(); // previous/past run distance from user input
+var pAvgPaceG = []; //pAvgePaceG is initally in seconds, then turns into MM:SS in final function call as FinalAdjustedPaceG
+
+var pWindmphG = $("#p-wind-mph").val(); //these will be the values from the form input
+var pPacemphG = (60 / pAvgPaceG);
+var pWindSecOffsetG;
+var pPaceRangeG = []; //holds low and high estimates for added seconds per mile due to heat in array
 
 
-var pRunTime = (pHours * 60) + (pMinutes * 60) + (pSeconds); // past run time from user input, probably need the $($stuff)
-//might change this to document.dataform.phours etc
-var pRunDist; // past run distance from user input
+var pTempG = $("#p-temp").val();
+var pDewG = $("#p-dew").val();
+var pHeatSecOffsetG = [];
+
+var FinalAdjustedPaceG = []; // final adjusted paces in MM:SS-MM:SS
+
+// CURRENT RUN TIME, DISTANCE, WEATHER
+
+
+var cWindmphG; //FROM API CALL
+var cPacemphG; (60 / cAvgPaceG);
+var cWindSecOffsetG;
+
+var cTempG; //FROM API CALL
+var cDewG;
+
 
 //_________________________________________________________________
 //BASIC PACE CALCULATOR
 //---------———————————————————————————————————————————–––––––––––––
-var distance; //made new generiic variables for the function paceCalc
-var hours;
-var minutes;
-var seconds;
-function paceCalc (distance, hours, minutes, seconds) {
-  distance = parseFloat(distance), hours = parseFloat(hours), 
-  minutes = parseFloat(minutes), seconds = parseFloat(seconds);
-    var avgPace;
-    var totalTime = 0;
+
+function paceCalc (distance, hours, minutes, seconds, globals) {
+  var distance = parseFloat(distance); //this will come from global variable from form, not (distance)
+  var hours = parseFloat(hours); //this will come from global variable from form, not (distance)
+  var minutes = parseFloat(minutes);//this will come from global variable from form, not (distance)
+  var seconds = parseFloat(seconds);//this will come from global variable from form, not (distance)
+  var totalTime = 0;
     totalTime += hours*3600;
     totalTime += minutes*60;
     totalTime += seconds;
-    var cpPace = Math.floor(totalTime/distance);
-    var paceMins = Math.floor (cpPace/60);
-    var paceSecs = cpPace - (paceMins*60);
-    if (paceSecs < 10) {
-        paceSecs = '0'+ paceSecs;
-      }
-    avgPace = paceMins + ":" + paceSecs;
-    return avgPace;
+    globals = totalTime;
+    // return pAvgPaceG in seconds don't need to return, this adjusts the global varaible average pace
   }
   console.log(paceCalc(13.1,1,35,18));
   paceCalc(26.2,3,29,09); //variables with user input will go in here
@@ -46,25 +59,15 @@ function paceCalc (distance, hours, minutes, seconds) {
 //_________________________________________________________________
 //WIND MPH TIME OFFSET - IN SECONDS PER MILE
 //---------———————————————————————————————————————————–––––––––––––
-  var windMPH1 = 20; //these will be the values from the form input
-  var pace1 = 8.2;
-  
-  //use different names for vars from input form and things passing into function 
-  function windOffset(windMPH, paceMPH){
-    //winOffset will return the wind's impact on runner's time seconds per mile. Later
+function windOffset(windMPH, paceMPH, globals){
+    //winOffset will return the wind's impact on runner's time seconds per mile.
     //Next we'll add this to the heat/temp offset
-    var windSecOffset;
-    var windDivPace = windMPH/paceMPH; //step-by-step so Math.pow doesn't freak out
-  // return (12*(Math.pow(windDivPace ,2)));
-  var windSecOffset = (12*(Math.pow(windDivPace ,2)));
-  return windSecOffset;
+  var windDivPace = windMPH/paceMPH; //step-by-step so Math.pow doesn't freak out
+  globals = (12*(Math.pow(windDivPace ,2)));
+  // return windSecOffset; DO NOT NEED, THIS IS GLOBAL
   }
-  console.log("wind offset s/mi: " + windOffset(windMPH1,pace1));
-  windOffset(windMPH1,pace1); // w/ vars from input form (listed above)
-  //THIS MEANS THAT WE CAN USE THE SAME "BASE" FUNCTION TO CALC OFFSET AT RACE TIME AND TODAY
-
 //_________________________________________________________________
-//HEAT TIME OFFSET - IN SECONDS PER MILE
+//HEAT TIME OFFSET - IN PERCENT PER MILE
 //---------———————————————————————————————————————————–––––––––––––
 // 100 or less:   no pace adjustment
 // 101 to 110:   0% to 0.5% pace adjustment
@@ -76,54 +79,60 @@ function paceCalc (distance, hours, minutes, seconds) {
 // 161 to 170:   6.0% to 8.0% pace adjustment
 // 171 to 180:   8.0% to 10.0% pace adjustment
 // Above 180:   hard running not recommended
-//x >= 0.001 && x <= 0.009
+//For version that calculates the new pace, see repl: https://repl.it/@mhanley00/Run-Pace-Head-Adj
+function heatEffect(temperature, dewPoint, basePace, globals){
 var temperature;
 var dewPoint;
 var basePace;
-function heatEffect(temperature, dewPoint, basePace){
   var heatScore = temperature + dewPoint;
   if (heatScore <= 100) {
-    var lowHeatEst = basePace;
-    var highHeatEst = basePace;
+    var lowHeatEst = 0;
+    var highHeatEst = 0;
   } 
   else if (heatScore >=101 && heatScore <=110){
-    var lowHeatEst = basePace;
-    var highHeatEst = (basePace*.005) + basePace;
+    var lowHeatEst = 0;
+    var highHeatEst = basePace*.005;
   }
   else if (heatScore >=111 && heatScore <=120){
-    var lowHeatEst = (basePace*.005) + basePace;
-    var highHeatEst = (basePace*.01) + basePace;
+    var lowHeatEst = basePace*.005;
+    var highHeatEst = basePace*.01;
   }
   else if (heatScore >=121 && heatScore <=130){
-    var lowHeatEst = (basePace*.01) + basePace;
-    var highHeatEst = (basePace*.02) + basePace;
+    var lowHeatEst = basePace*.01;
+    var highHeatEst = basePace*.02;
   }
   else if (heatScore >=131 && heatScore <=140){
-    var lowHeatEst = (basePace*.02) + basePace;
-    var highHeatEst = (basePace*.03) + basePace;
+    var lowHeatEst = basePace*.02;
+    var highHeatEst = basePace*.03;
   }
   else if (heatScore >=141 && heatScore <=150){
-    var lowHeatEst = (basePace*.03) + basePace;
-    var highHeatEst = (basePace*.045) + basePace;
+    var lowHeatEst = basePace*.03;
+    var highHeatEst = basePace*.045;
   }
   else if (heatScore >=151 && heatScore <=160){
-    var lowHeatEst = (basePace*.045) + basePace;
-    var highHeatEst = (basePace*.06) + basePace;
+    var lowHeatEst = basePace*.045;
+    var highHeatEst = basePace*.06;
   }
   else if (heatScore >=161 && heatScore <=170){
-    var lowHeatEst = (basePace*.06) + basePace;
-    var highHeatEst = (basePace*.08) + basePace;
+    var lowHeatEst = basePace*.06;
+    var highHeatEst = basePace*.08;
   }
   else if (heatScore >=171 && heatScore <=180){
-    var lowHeatEst = (basePace*.08) + basePace;
-    var highHeatEst = (basePace*.1) + basePace;
+    var lowHeatEst = basePace*.08;
+    var highHeatEst = basePace*.1;
   }
   else if (heatScore >=180){
     return "Head to the pool!" //write to DOM
   }
-  return lowHeatEst + " – " + highHeatEst;
+  // return lowHeatEst + " – " + highHeatEst;
+  // return highHeatEst + " – " + lowHeatEst;
+  globals.push(lowHeatEst, highHeatEst);
+  // return paceRange;
+
 }
-console.log(heatEffect(78,58,420));
+console.log(heatEffect(20,100, 100));
+heatEffect(20,100, 1000);
+
 
 //_________________________________________________________________
 //AG% CALCULATOR
@@ -131,6 +140,7 @@ console.log(heatEffect(78,58,420));
 var gender; // gender male/female from user input
 var ages = new Array(5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100);
 var pAGP; // AG% of past race time, calculated/ assinged to function AGCalc
+var gender;
 var womens = {
   OC:{'5km': [3.1, 886]},
   '50': {'5km':[3.1, 0.8937],	
@@ -138,9 +148,8 @@ var womens = {
   '60': {'5km':[5, 0.77], 
   '6km' :[6, .777]}
 };
-var gender;
-console.log("world record time: " + womens.OC['5km'][1]); //this now returns 886 🙌
-function AGCalc (gender, age, distance, ageRunTime) {
+//console.log("world record time: " + womens.OC['5km'][1]); //this now returns 886 🙌
+function AGCalc (gender, age, distance, ageRunTime, globals) {
 age = parseFloat(age), ageRunTime = parseFloat(ageRunTime);
 if (gender === 'm'){
   var offset = mens[age][distance][1];
@@ -150,9 +159,35 @@ if (gender === 'm'){
 if (gender === 'f'){
   var offset = womens[age][distance][1];
   var AGadj = (womens['OC'][distance][1])/offset;
-  var pAGP = AGadj/ageRunTime;
+  globals = AGadj/ageRunTime;
 }
 return pAGP;
 }
 console.log("Your AG% is: " +AGCalc('f', 50, '5km', 3000));
  AGCalc('f', 50, '5km', 2000);
+
+//_________________________________________________________________
+//INITIALIZE VARIABLES (ie giant callback)
+//---------———————————————————————————————————————————–––––––––––––
+ function initVars(){
+  //this will go at end; grab all variables from form, make global variables
+  paceCalc (pRunDistG, pHoursG, pMinutesG, pSecondsG, pAvgPaceG); //  paceCalc (pRunDistG, pHoursG, pMinutesG, pSecondsG, cPaceRangeG);
+  windOffset(pWindmphG, pPacemphG, pWindSecOffsetG);
+  heatEffect(pTempG, pDewG);
+}
+
+function adjustedPace (basePace, windOffsetSeconds, paceRange) {
+  var windAdjustedPace = basePace + windOffsetSeconds;
+  for (var i = 0; i< paceRange.length, i++) {
+  FinalAdjustedPaceG.push(windAdjustedPace + paceRange[i]);
+  }
+}
+//jquery document ready
+initVars().then(adjustedPace(pAvgPaceG, pWindSecOffsetG, pPaceRangeG)); //make sure these are all global
+//probably need separate global vars for past and current run times/weather
+
+function initCurrentVars(){
+  
+}
+
+adjustedPace(pAvgPaceG, cWindSecOffsetG, cPaceRangeG);
